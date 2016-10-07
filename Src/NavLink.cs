e6833172484458c8d02385efd89ce61fac72e58e
Roman Starkov <sourcecode@roman.st>
2016-10-07 02:50:28 +0100

    Simplify ancient C#

--- Src/NavLink.cs
+++ Src/NavLink.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace RT.Spinneret
+﻿namespace RT.Spinneret
 {
     public class NavLink
     {
-        private string _section;
-        private string _text;
-        private string _href;
         public NavLink(string section, string text, string href)
         {
-            _section = section;
-            _text = text;
-            _href = href;
+            Section = section;
+            Text = text;
+            Href = href;
         }
-        public string Section { get { return _section; } }
-        public string Text { get { return _text; } }
-        public string Href { get { return _href; } }
+
+        public string Section { get; private set; }
+        public string Text { get; private set; }
+        public string Href { get; private set; }
     }
 }
